2e362a078c4cec78685f924485fdad354106c3c2
vladimirovmm <vladimirov.m.m@mail.ru>
2025-04-22 16:17:19 +0700

    feat(primitive_types): any

--- sandbox_std/exp_std/src/exp_any.rs
+++ sandbox_std/exp_std/src/exp_any.rs
@@ -19,29 +19,42 @@ fn test_box_dyn() {
     /// Эта функция лишь демонстрация работы с dyn Any. Понятнее и удобнее в использование реализация через Trait.
     fn multi_add<T: Any>(back_result: isize, value: &T) -> isize {
         let value_any = value as &dyn Any;
+        println!("type name: {}", std::any::type_name::<T>());
 
-        let val = if let Some(val) = value_any.downcast_ref::<i8>() {
+        multi_add2(back_result, value_any)
+    }
+    /// Разбито на отдельную функцию просто для демонстрации возможности и избежания повторения кода.
+    fn multi_add2(back_result: isize, value: &dyn Any) -> isize {
+        println!("type name by value: {}", std::any::type_name_of_val(value));
+
+        if value.is::<String>() {
+            println!("Cтрока: {}", value.downcast_ref::<String>().unwrap());
+        }
+
+        let val = if let Some(val) = value.downcast_ref::<i8>() {
             *val as isize
-        } else if let Some(val) = value_any.downcast_ref::<u8>() {
+        } else if let Some(val) = value.downcast_ref::<u8>() {
             *val as isize
-        } else if let Some(val) = value_any.downcast_ref::<&str>() {
+        } else if let Some(val) = value.downcast_ref::<&str>() {
             val.parse().unwrap_or_default()
-        } else if let Some(val) = value_any.downcast_ref::<char>() {
+        } else if let Some(val) = value.downcast_ref::<String>() {
+            val.parse().unwrap_or_default()
+        } else if let Some(val) = value.downcast_ref::<char>() {
             val.to_string().parse().unwrap_or_default()
         } else {
             return back_result;
         };
-
         back_result + val
     }
-
     let mut result = 0;
     result = multi_add(result, &1_i8);
     result = multi_add(result, &2_u8);
     result = multi_add(result, &"3");
     result = multi_add(result, &'4');
-    result = multi_add(result, &"null");
-    result = multi_add(result, &["null"]);
+    result = multi_add(result, &"null"); // Некорректное значение.
+    result = multi_add(result, &("5".to_string()));
+    result = multi_add(result, &"5-..".to_string()); // Некорректное значение.
+    result = multi_add(result, &["null"]); // Некорректное значение.
 
-    assert_eq!(result, 10);
+    assert_eq!(result, 15);
 }
